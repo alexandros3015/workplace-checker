bfcbef7622d94e1c47dce06d6b1f0c3573fb9f28
alexandros3015 <105521378+alexandros3015@users.noreply.github.com>
2025-02-17 19:06:51 +0000

    THe

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import google.generativeai as genai
-import os
 
-genai.configure(api_key="Delevopment")
+company = input("Enter Company name: ")
 
-model = genai.GenerativeModel('gemini-1.5-flash')
-response = model.generate_content("The opposite of cold is: RETURN A JSON RESPONSE")
+genai.configure(api_key="")
+
+model = genai.GenerativeModel('gemini-1.5-flash', generation_config={
+    "response_mime_type":"application/json",
+    "temperature": 0
+})
+
+response = model.generate_content(
+    f"""
+    Given an input company name, return its canonical corporate entity name (using the parent company name if it represents a division—e.g., "Amazon Arizona help desk" becomes "Amazon"—but if it's a standalone company like "McDonalds paint supply", return it as provided) and a boolean flag that is true if the input exactly matches a verified corporate entity (e.g., "McDonalds" returns true) and false if it does not (e.g., "driver" returns false).
+
+    Ensure you fix any spelling errors
+    The name of the company/job goes into name, and the verification status goes into needVerify
+
+    Return like:
+
+        \"companies\": [name: string, needVerify: boolean] 
+        
+
+    Your provided compnay name is: {company}
+
+    """
+)
+
 print(response.text)
